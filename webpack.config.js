60b1e29753b4a21254bc239ba8ffa4e0498e9335
maphnew <zcm3118@gmail.com>
2021-10-17 18:50:31 +0900

    MiniCssExtract Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const webpack = require('webpack')
+const childProcess = require('child_process')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
@@ -15,7 +19,9 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    process.env.NODE_ENV === 'production'
+                    ? MiniCssExtractPlugin.loader
+                    : 'style-loader',
                     'css-loader'
                 ]
             },
@@ -23,7 +29,7 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)/,
                 loader: 'url-loader',
                 options: {
-                    publicPath: './dist/', 
+                    // publicPath: './dist/', 
                     name: '[name].[ext]?[hash]',
                     limit: 20000, // 20kb
                 }
@@ -33,8 +39,30 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin({
             banner: `
-                
+                Build Date: ${new Date().toLocaleString()}
+                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+                Author: ${childProcess.execSync('git config user.name')}
             `
-        })
+        }),
+        new webpack.DefinePlugin({
+            TWO: JSON.stringify('1+1'),
+            'api.domain': JSON.stringify('http://dev.api.domain.com')
+        }),
+        new HtmlWebpackPlugin({
+            hash: true, 
+            template: './src/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === 'development' ? '(개발용)': ''
+            },
+            minify: process.env.NODE_ENV === 'production' ? {
+                collapseWhitespace: true,
+                removeComments: true
+            } : false
+        }),
+        new CleanWebpackPlugin(),
+        ...(process.env.NODE_ENV === 'production'
+            ? [new MiniCssExtractPlugin({filename: '[name].css'})]
+            : []
+        )
     ]
 }
